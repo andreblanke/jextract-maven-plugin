ac37540350e69186f7da4f6f463216c3c7b846d6
Andre Blanke <blanke.and@gmail.com>
2022-06-16 12:25:04 +0200

    Extend tests with more assertions

--- src/test/java/dev/blanke/maven/plugins/JextractMojoIT.java
+++ src/test/java/dev/blanke/maven/plugins/JextractMojoIT.java
@@ -7,19 +7,27 @@ import com.soebes.itf.jupiter.maven.MavenExecutionResult;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.soebes.itf.extension.assertj.MavenExecutionResultAssert.assertThat;
 
 @MavenJupiterExtension
-class JextractMojoIT {
+final class JextractMojoIT {
 
     @MavenTest
     void point(final MavenExecutionResult result) {
-        assertTrue(result.isSuccessful());
+        assertThat(result).isSuccessful()
+            .project()
+            .hasTarget()
+                .withJarFile()
+                .containsOnlyOnce("org/jextract/point/point_h.class");
     }
 
     @MavenTest
-    @EnabledOnOs({ OS.LINUX, OS.MAC })
+    @EnabledOnOs(OS.LINUX)
     void unistd(final MavenExecutionResult result) {
-        assertTrue(result.isSuccessful());
+        assertThat(result).isSuccessful()
+            .project()
+            .hasTarget()
+                .withJarFile()
+                .containsOnlyOnce("org/unix/unistd/unistd_h.class");
     }
 }
